96a4d8bcc867db47d2388e37b4f17487ecf58c34
Ruben Romero Montes <rromerom@redhat.com>
2026-06-02 11:48:03 +0200

    chore: implement tests to increase coverage and exclude generated sources
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- src/test/java/io/github/guacsec/trustifyda/api/PackageRefTest.java
+++ src/test/java/io/github/guacsec/trustifyda/api/PackageRefTest.java
@@ -18,11 +18,12 @@
 package io.github.guacsec.trustifyda.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
-import io.github.guacsec.trustifyda.api.PackageRef;
 
 public class PackageRefTest {
 
@@ -65,5 +66,91 @@ public class PackageRefTest {
     assertEquals(originalPurl, ref.toString());
 
     assertTrue(ref.isCoordinatesEquals(new PackageRef(coordinates)));
+    assertFalse(ref.isCoordinatesEquals(null));
+  }
+
+  @Test
+  void builderBuildsMavenPurlFromCoordinates() {
+    var ref = PackageRef.builder()
+        .pkgManager("maven")
+        .namespace("org.example")
+        .name("artifact")
+        .version("1.0.0")
+        .build();
+    assertEquals("pkg:maven/org.example/artifact@1.0.0", ref.toString());
+    assertEquals("org.example:artifact", ref.name());
+    assertEquals("1.0.0", ref.version());
+  }
+
+  @Test
+  void builderAcceptsRawPurl() {
+    var purl = "pkg:npm/foo@1.2.3";
+    var ref = PackageRef.builder().purl(purl).build();
+    assertEquals(purl, ref.ref());
+  }
+
+  @Test
+  void builderRejectsInvalidPurl() {
+    assertThrows(IllegalArgumentException.class,
+        () -> PackageRef.builder().purl("not-a-purl").build());
+  }
+
+  @Test
+  void builderRequiresCoordinatesWhenPurlIsAbsent() {
+    assertThrows(NullPointerException.class,
+        () -> PackageRef.builder().pkgManager("maven").name("x").build());
+  }
+
+  @Test
+  void stringConstructorRejectsInvalidPurl() {
+    assertThrows(IllegalArgumentException.class, () -> new PackageRef("not-a-purl"));
+  }
+
+  @Test
+  void parseBuildsRefFromFourPartGav() {
+    var ref = PackageRef.parse("com.example:my-artifact:jar:1.2.3", "maven");
+    assertEquals("pkg:maven/com.example/my-artifact@1.2.3", ref.toString());
+    assertEquals("com.example:my-artifact", ref.name());
+  }
+
+  @Test
+  void parseBuildsRefFromSixPartGav() {
+    var ref = PackageRef.parse("com.example:my-artifact:jar:extra:2.0.0:notes", "maven");
+    assertEquals("pkg:maven/com.example/my-artifact@2.0.0", ref.toString());
+    assertEquals("2.0.0", ref.version());
+  }
+
+  @Test
+  void parseRejectsUnexpectedGavFormat() {
+    assertThrows(IllegalArgumentException.class,
+        () -> PackageRef.parse("it:has:more:than:six:parts:too:many", "maven"));
+    assertThrows(IllegalArgumentException.class,
+        () -> PackageRef.parse("only:three:parts", "maven"));
+  }
+
+  @Test
+  void toGavAndUrlQueryString() {
+    var ref = new PackageRef("pkg:maven/org.example/artifact@1.0.0");
+    assertEquals("org.example:artifact:1.0.0", ref.toGav());
+    assertEquals("purl=pkg:maven/org.example/artifact@1.0.0", ref.toUrlQueryString("purl"));
+    assertEquals("=pkg:maven/org.example/artifact@1.0.0", ref.toUrlQueryString(null));
+  }
+
+  @Test
+  void equalsAndHashCodeUseFullPurl() {
+    var a = new PackageRef("pkg:maven/org.example/artifact@1.0.0");
+    var b = new PackageRef("pkg:maven/org.example/artifact@1.0.0");
+    var c = new PackageRef("pkg:maven/org.example/artifact@2.0.0");
+    assertEquals(a, b);
+    assertEquals(a.hashCode(), b.hashCode());
+    assertNotEquals(a, c);
+    assertFalse(a.equals(null));
+    assertFalse(a.equals(new Object()));
+  }
+
+  @Test
+  void nonMavenNameJoinsNamespaceWithSlash() {
+    var ref = new PackageRef("pkg:golang/example.com/lib@v1.0.0");
+    assertEquals("example.com/lib", ref.name());
   }
 }
